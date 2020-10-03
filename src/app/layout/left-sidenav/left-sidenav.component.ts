12a10823c0f1792762bdfd0c08353f4e88715ffb
vaalgrind <pawel.tomkowski@silevis.com>
2020-10-03 17:45:56 +0200

    Add leftsidenav animations.

--- src/app/layout/left-sidenav/left-sidenav.component.ts
+++ src/app/layout/left-sidenav/left-sidenav.component.ts
@@ -1,15 +1,38 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { LayoutService } from 'src/app/modules/core/layout.service';
 
 @Component({
   selector: 'app-left-sidenav',
   templateUrl: './left-sidenav.component.html',
-  styleUrls: ['./left-sidenav.component.scss']
+  styleUrls: ['./left-sidenav.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state(
+        'in',
+        style({
+          width: '200px',
+        })
+      ),
+      state(
+        'out',
+        style({
+          width: '0',
+        })
+      ),
+      transition('in => out', animate('200ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out')),
+    ]),
+  ],
 })
 export class LeftSidenavComponent implements OnInit {
+  constructor(public layoutService: LayoutService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
